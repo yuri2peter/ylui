73b21c5b7676bebd1f51b9c9e32180fc12ffbf3a
Yuri2 <824831811@qq.com>
2018-05-21 22:04:31 +0800

    添加捐助图片

--- res/js/yl-render.js
+++ res/js/yl-render.js
@@ -424,7 +424,7 @@ YL.render = function (data) {
                 if (first && !YL.static.serialNumber){
                     YL.onReady(function () {
                       setTimeout(function () {
-                        YL.msg("YLUI v"+YL.info.version+" 社区版",'当前YLUI为社区版，仅限个人用户学习使用，禁止商用及企业使用。<br/>更多信息及获取商业授权请访问:<br/>YLUI官网：'+'<a style="color: #2e8ded" target="_blank" href="https://ylui.yuri2.cn">https://ylui.yuri2.cn</a>');
+                          YL.msg("YLUI v"+YL.info.version+" 社区版",'当前YLUI为社区版，仅限个人用户学习使用，禁止商用及企业使用。<br/>更多信息及获取商业授权请访问:<br/>YLUI官网：'+'<a style="color: white" target="_blank" href="https://ylui.yuri2.cn">https://ylui.yuri2.cn</a><p>欢迎使用支付宝扫描二维码对作者进行捐赠，表达您对YLUI的支持~<br/><br/><img style="width: 100%" src="./res/img/donation.png"/></p>');
                       },1500)
                     });
                 }
