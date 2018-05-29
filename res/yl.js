565c5eca21304e8f4f9a47a92e3f1ce7a7f238ce
hanlin.tang <hanglin.tang@buttoninvest.com>
2018-05-29 17:30:43 +0800

    2.0.4

--- res/yl.js
+++ res/yl.js
@@ -1,7 +1,7 @@
 window.YL = {
     info: {
         softwareName: 'YLUI',
-        version: "2.0.3",
+        version: "2.0.4",
         iconBtnStart: 'yoast',
         author: 'Yuri2',
         contactInformation: 'yuri2peter@qq.com',
